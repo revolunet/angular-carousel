ae12989bbc41b1012e7259bc98eb044a8fa2ea5c
Julien Bouquillon <julien@bouquillon.com>
2013-04-22 18:26:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# AngularJS Touch Carousel
+
+This is a simple carousel implementation for AngularJS ng-repeats.
+
+For best performances on mobile devices, it uses CSS 3D transforms (GPU accel).
+
+It handles click and touch events.
+
+Demo : http://revolunet.github.io/angular-carousel
+
+## Usage :
+
+Add a dependency to `angular-carousel` module in your application.
+
+Then, just add a `carousel` attribute to your `<ul>` block and your `<li>`'s become swipable ;)
+
+```html
+<h3>Discover sports</h3>
+<ul carousel class="image">
+  <li ng-repeat="image in sportImages" style="background-image:url({{ image }});">
+    <div class="layer">{{ image }}</div>
+  </li>
+</ul>
+```
+
+Of course, include `angular-carousel.js` and `angular-carousel.css` in your project.
+
+## Todo :
+ - Tests :)
+ - cancel swipe if too short
+ - indicator
+ - buffer slides (3) to reduce DOM size and allow dynamic add/remove of slides (inifinite scroll)
+
+## Inspirations
+ - https://github.com/ajoslin/angular-mobile-nav
+ - http://mobile.smashingmagazine.com/2012/06/21/play-with-hardware-accelerated-css/
+ - Thanks @ganarajpr for support
+
+## Licence
+As AngularJS itself, this module is released under the permissive [MIT license](http://revolunet.mit-license.org). Your contributions are always welcome.
---
+++ src/angular-carousel.js
@@ -0,0 +1,159 @@
+"use strict";
+
+/*
+Angular touch carousel with CSS GPU accel
+http://github.com/revolunet/angular-carousel
+*/
+
+angular.module('angular-carousel', [])
+
+  .directive('carousel', function($document) {
+    // track number of carousel instances
+    var carousels = 0;
+    return {
+      restrict: 'A',
+      scope: true,
+      controller: function($scope, $element, $attrs) {
+        /*
+          listen for item add/remove in the ng-repeat
+          resizes container based on the first slide added
+        */
+        var nbItems = 0;
+        var width = 0;
+        this.addElement = function(elm){
+          nbItems++;
+          if (nbItems===1) {
+            // auto resize the container to fit the first slide
+            width = elm[0].getBoundingClientRect().width;
+            // todo : 2px substrcat because of some mobile viewports weirds behaviours (android)
+            $element.parent().css('width', (this.getWidth() - 2) + 'px');
+          }
+        };
+        this.removeElement = function(elm){
+          nbItems--;
+        };
+        this.getWidth = function() {
+          return width;
+        };
+      },
+      compile: function(tElement, tAttrs) {
+
+        tElement.addClass('carousel-slides');
+        tElement.find('li').attr('slide-announcer', true);
+
+        return function(scope, iElement, iAttrs, controller) {
+          // init some variables
+          carousels++;
+          var carouselId = 'carousel-' + carousels;
+          var swiping = 0,
+              startX = 0,
+              startOffset  = 0,
+              offset  = 0,
+              curSlide = 0;
+
+          // add a wrapper div that will hide overflow
+          var carousel = iElement.wrap("<div id='" + carouselId +"' class='carousel-container'></div>"),
+              container = carousel.parent();
+
+          // todo : cannot access this controller method from a bound event ? #WTF
+          container.getWidth = controller.getWidth;
+
+          var getSlides = function() {
+              /* returns items in the carousel */
+              return carousel.find('li');
+          };
+
+          var getSlidesCount = function() {
+              /* returns the detected number of items in the carousel */
+              return getSlides().length;
+          };
+
+          var transformEvent = function(event) {
+            /* allow mouseEvent + touchEvent */
+            if ((typeof event.originalEvent !== 'undefined') && event.originalEvent.touches > 0)
+              event = event.originalEvent.touches[0];
+            else if ((typeof event.touches !== 'undefined') && event.touches.length > 0)
+              event = event.touches[0];
+            return event;
+          };
+
+          var swipeStart = function(event) {
+            /* capture initial event position */
+            event = transformEvent(event);
+            if (swiping === 0) {
+              swiping = 1;
+              startX = event.clientX;
+            }
+          };
+
+          var swipe = function(event) {
+            /* follow cursor movement */
+            if (swiping===0) return;
+            event.preventDefault();
+            event = transformEvent(event);
+
+            var deltaX = event.clientX - startX;
+            if (swiping == 1 && deltaX !== 0) {
+              swiping = 2;
+              startOffset = offset;
+            }
+            else if (swiping == 2) {
+              var slideCount = getSlidesCount();
+              // ratio is used for the 'rubber band' effect
+              var ratio = 1;
+              if ((curSlide === 0 && event.clientX > startX) || (curSlide == slideCount - 1 && event.clientX < startX))
+                ratio = 3;
+              offset = startOffset + deltaX / ratio;
+              carousel.css({
+                '-webkit-transform': 'translate3d(' + offset + 'px,0,0)'
+              }).removeClass().addClass('carousel-noanimate');
+            }
+          };
+
+          var swipeEnd = function(event) {
+            /* when movement ends, go to next slide or stay on the same */
+            var slideCount = getSlidesCount();
+            if (swiping > 0) {
+              swiping = 0;
+              curSlide = offset < startOffset ? curSlide + 1 : curSlide - 1;
+              curSlide = Math.min(Math.max(curSlide, 0), slideCount - 1);
+              var slideWidth = container.getWidth();
+              offset = curSlide * -slideWidth;
+              carousel.removeClass('carousel-noanimate').addClass('carousel-animate').css('-webkit-transform', 'translate3d(' + offset + 'px,0,0)');
+            }
+          };
+
+          var mouseExit = function(event) {
+            // todo : cancel swipe if going outside the area
+            console.log('mouseExit', event, event.targetElement);
+          };
+
+          // bind events
+          container.bind('mousedown touchstart', swipeStart);
+          container.bind('mousemove touchmove', swipe);
+          container.bind('mouseup touchend', swipeEnd);
+          carousel.bind('mouseleave', mouseExit, true);
+
+          scope.destroy = function(callback) {
+              // todo
+          };
+        };
+      }
+    };
+  })
+  .directive('slideAnnouncer', function() {
+    /* listen to modifications on the ng-repeat */
+    return {
+      restrict: 'A',
+      require : '^carousel',
+      link: function(scope, element, attrs, slideAnnouncerCtrl) {
+          if(!slideAnnouncerCtrl){
+              return;
+          }
+          slideAnnouncerCtrl.addElement(element);
+          scope.$on("$destroy",function(){
+             slideAnnouncerCtrl.removeElement(element);
+          });
+      }
+    };
+  });
