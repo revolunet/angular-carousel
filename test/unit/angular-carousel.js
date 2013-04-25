22af3b2049ba15c39447984a21d79d847a943f1f
Julien Bouquillon <contact@revolunet.com>
2013-04-25 12:35:48 +0300

    style(tests): cleanup

--- test/unit/angular-carousel.js
+++ test/unit/angular-carousel.js
@@ -1,4 +1,4 @@
-/*global beforeEach, afterEach, describe, it, inject, expect, module, spyOn, fullcalendar, angular, $*/
+/*global beforeEach, afterEach, describe, it, inject, expect, module, angular, $*/
 
 describe('carousel', function () {
   'use strict';
@@ -55,81 +55,5 @@ describe('carousel', function () {
         scope.$digest();
         expect(elm.find('li[slide-announcer=true]').length).toBe(4);
     });
-    it('should add announcer attribute to each slide 2', function () {
-        var elm = compileTpl();
-        expect(elm.find('li[slide-announcer=true]').length).toBe(3);
-    });
   });
-
-  // function compileDirective(template) {
-  //   template = template ? templates[template] : templates['default'];
-  //   angular.extend(scope, template.scope);
-  //   var $element = $(template.element).appendTo($sandbox);
-  //   $element = $compile($element)(scope);
-  //   scope.$digest(); // evaluate $evalAsync queue used by $q
-  //   $timeout.flush();
-  //   return $element;
-  // }
-
-  // describe('default template', function() {
-
-  //   var elm, select, menu;
-  //   beforeEach(function() {
-  //     elm = compileDirective();
-  //     select = elm.next('.bootstrap-select');
-  //     menu = select.find('ul[role=menu]');
-  //   });
-
-  //   it('initialises bootstrap select on the element', function () {
-  //     expect(select.length).toBe(1);
-  //   });
-
-  //   it('adds every item to the bootstrap select menu', function () {
-  //     expect(menu.children().length).toBe(scope.items.length);
-  //   });
-
-  //   it('updates the bootstrap select menu when items are changed', function () {
-  //     scope.items.push({id: '4', name: 'qux'});
-  //     scope.$digest();
-  //     expect(menu.children().length).toBe(scope.items.length);
-  //   });
-
-  //   it('selects the correct item by default', function () {
-  //     expect(menu.find('.selected').text()).toBe('bar');
-  //   });
-
-  //   it('updates the scope when a new item is selected', function () {
-  //     menu.find('li a').first().click();
-  //     expect(scope.selectedItem).toBe('1');
-  //   });
-
-  //   it('updates bootstrap select when the model changes', function () {
-  //     scope.selectedItem = '3';
-  //     scope.$digest();
-  //     expect(menu.find('.selected').text()).toBe('baz');
-  //   });
-
-  //   it('does not add ng-scope class to bootstrap select element', function () {
-  //     expect(select.hasClass('ng-scope')).toBe(false);
-  //   });
-
-    // it('adds new classes from original element when the model changes', function () {
-    //   elm.addClass('dummy');
-    //   scope.model.item = 1;
-    //   scope.$digest();
-    //   expect(select.hasClass('dummy')).toBe(true);
-    // });
-
-    // it('syncs classes removed from original element when the model changes', function () {
-    //   element.addClass('dummy');
-    //   scope.model.item = 1;
-    //   scope.$digest();
-    //   element.removeClass('dummy');
-    //   scope.model.item = 2;
-    //   scope.$digest();
-    //   expect(select.hasClass('dummy')).toBe(false);
-    // });
-
- // });
-
 });
