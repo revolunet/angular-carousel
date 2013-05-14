b8bb4eb5ca9b8bfe451ca1fbcc47fbbfce2c2d18
Julien Bouquillon <julien@bouquillon.com>
2013-05-14 10:59:36 +0200

    bump version

--- test/unit/angular-carousel.js
+++ test/unit/angular-carousel.js
@@ -1,4 +1,4 @@
-/*global beforeEach, afterEach, describe, it, inject, expect, module, angular, $*/
+/*global browserTrigger, beforeEach, afterEach, describe, it, inject, expect, module, angular, $*/
 
 describe('carousel', function () {
   'use strict';
