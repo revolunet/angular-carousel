c5cff15f0342ef0b445c57f8d1955ae3bcc80c41
Antonio Morales García <antonio@intelligenia.com>
2014-03-13 10:19:15 +0100

    IE9 support, so the carrousel will work on IE9. Optional parameter IE9Support will enable this support. If enabled, the directive will check if the browser is IE9 and, in this case, will use translate instead of translate3d and simulate requestAnimationFrame with a timeout
    Version tag: 0.2.0+IE9Support-rafPolyFill

--- src/directives/rn-carousel.js
+++ src/directives/rn-carousel.js
@@ -194,7 +194,7 @@
 
                         /* Check Internet Explorer 9 Compatibility */       
                         if( angular.isDefined(iAttributes.rnCarouselIe9support) && 
-                            $.browser.msie && parseFloat($.browser.version)<= 9
+                            has3d()
                         ){
                             carousel[0].style[transformProperty] = 'translate(' + move + 'px, 0)';
                         }
@@ -401,6 +401,33 @@
                         }
                         return true;
                     });
+
+                    //Detect support of translate3d
+                    function has3d(){
+                        var el = document.createElement('p'),
+                        has3d,
+                        transforms = {
+                            'webkitTransform':'-webkit-transform',
+                            'OTransform':'-o-transform',
+                            'msTransform':'-ms-transform',
+                            'MozTransform':'-moz-transform',
+                            'transform':'transform'
+                        };
+                     
+                        // Add it to the body to get the computed style
+                        document.body.insertBefore(el, null);
+                     
+                        for(var t in transforms){
+                            if( el.style[t] !== undefined ){
+                                el.style[t] = 'translate3d(1px,1px,1px)';
+                                has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+                            }
+                        }
+                     
+                        document.body.removeChild(el);
+                     
+                        return (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+                    }
 
                     function onOrientationChange() {
                         updateContainerWidth();
