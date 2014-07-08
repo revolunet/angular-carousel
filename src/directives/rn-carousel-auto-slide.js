62505b43b6114e15628f7d6ade6f7934afa80d55
Costica Puntaru <pc@everymatrix.com>
2014-07-08 15:10:29 +0300

    adding support for automatic slider and pause on hover

---
+++ src/directives/rn-carousel-auto-slide.js
@@ -0,0 +1,44 @@
+angular.module('angular-carousel')
+
+.directive('rnCarouselAutoSlide', ['$interval', function($interval) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+        var delay = Math.round(parseFloat(attrs.rnCarouselAutoSlide) * 1000),
+            timer = isPaused = increment = false;
+
+        stopAutoplay = function () {
+            if (angular.isDefined(timer)) {
+                $interval.cancel(timer);
+            }
+            timer = undefined;
+        };
+
+        increment = function () {
+            if(!isPaused){
+                if (scope.indicatorIndex < scope.carouselIndicatorArray.length - 1) {
+                    scope.indicatorIndex++;
+                } else {
+                    scope.indicatorIndex = 0;
+                }
+            }
+
+        };
+
+        timer = $interval(increment, delay);
+        console.log('attrs.rnCarouselPauseOnHover',attrs.rnCarouselPauseOnHover);
+        if (attrs.rnCarouselPauseOnHover && attrs.rnCarouselPauseOnHover != 'false'){
+            element.on('mouseenter', function(){
+                stopAutoplay();
+            });
+
+            element.on('mouseleave', function(){
+                timer = $interval(increment, delay);
+            });
+        }
+
+        scope.$on('$destroy', stopAutoplay);
+
+    }
+  };
+}]);
